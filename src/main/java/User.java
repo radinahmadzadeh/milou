cf9e6c2162bfd94db6dc2764c3fb5c97cd080e9b
Radin <radinrad372@yahoo.com>
2025-06-10 21:48:43 +0330

    complete email

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -19,9 +19,9 @@ public class User {
 
     public User() {}
 
-    public User(String name , String email , String password){
+    public User(String name, String email, String password) {
         this.name = name;
-        this.email = email;
+        this.email = email.endsWith("@milou.com") ? email : email + "@milou.com";
         this.password = password;
     }
 
